2db0e828fe189383a3ba84fd2124082bb78115fc
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-01-01 09:37:07 +0100

    First commit -and configuration setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <div className='h-screen w-full  bg-red-500 flex justify-center items-center'>
+        <h1 className="text-green-400">Hello</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
