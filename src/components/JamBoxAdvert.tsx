9f78bfa6d21ad400cef5ed465ee31940c672ad58
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-02-02 23:58:08 +0100

    jamboxadvert

--- src/components/JamBoxAdvert.tsx
+++ src/components/JamBoxAdvert.tsx
@@ -8,7 +8,6 @@ export const JamBoxAdvert = () => {
       <div className="block md:hidden">
         <img src={A} alt="" className="w-[1000px] h-auto" />
       </div>
-
       <div className="hidden md:block">
         <div className="flex justify-between text-white p-12 bg-black w-full md:h-[500px]">
           <div className="flex flex-col justify-between pt-5 gap-7">
@@ -18,13 +17,11 @@ export const JamBoxAdvert = () => {
             </h1>
             <div className="flex gap-9 items-center">
               <JamboxTimer duration={24 * 24 * 60 * 60 * 1000} />
-             
             </div>
             <div className="flex justify-center items-center rounded-md bg-[#00FF66] text-white text-center cursor-pointer md:w-[159px] md:h-[56px] h-[50px] w-[30vw]">
               Buy Now!
             </div>
           </div>
-
           <div className="w-[568px] h-[330px] flex items-center">
             <img src={box} alt="dnd" className="w-full h-full" />
           </div>
