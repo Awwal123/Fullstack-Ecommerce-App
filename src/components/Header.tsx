3f60530f688a0d0270633d3167277a9a9a8ad9d7
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-02-02 23:56:39 +0100

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 export const Header = () => {
     return (
         <div className="hidden md:block w-full p-3 text-white bg-black">
-                <p className="text-center text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+        <p className="text-center text-sm">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
         </div>
     )
 }
